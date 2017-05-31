1899e1849362dff5d473989b73aead9cca4f0066
EK Azubuike <ayo.azubuike@gmail.com>
2017-05-31 18:30:58 -0500

    Styled sketchpad, added erase and reset buttons, edited js

--- etch-sketch.js
+++ etch-sketch.js
@@ -2,15 +2,26 @@ $(document).ready(function(){
   $(".box").mouseenter(function(){
     $(this).addClass("highlight");
   });
-  $("button").click(function(){
+  
+  $(".erase").click(function(){
+    $(".box").removeClass("highlight");
+  });
+  
+  $(".reset").click(function(){
     $(".box").removeClass("highlight");
     var num = prompt("How big do you want your canvas?");
-    var container = $(".container").innerHTML;
+    var container = $(".container");
+    var newBox = $('<div class="box"></div>');
     if (num > 0) {
-      $(".container").empty();
-      for (var i = 0; i < num; i++) {
-        container += "<p>Hi!</p><br>";
+      $(".box").remove();
+      for (var i = 0; i < num * num; i++) {
+        container.append(newBox.clone());
       }
+      $(".box").mouseenter(function(){
+        $(this).addClass("highlight");
+      });
+      var size = (100/num) + "%";
+      $(".box").css({"width" : size, "height": size});
     } else {
       alert("Try again with a value greater than 0!");
     }
